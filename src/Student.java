30db9b43a823ed2acc6c0ed9b016ddf86e92800e
Tanase Sergiu <tanasesergiu15@gmail.com>
2026-03-11 15:15:20 +0200

    Rezolvare Lab2 Ex2.

--- src/Student.java
+++ src/Student.java
@@ -2,16 +2,34 @@ public class Student {
     private int numarMatricol;
     private String prenume;
     private String nume;
-    private int formatieDeStudiu;
+    private String formatieDeStudiu;
 
-    public Student(int numarMatricol, String prenume, String nume, int formatieDeStudiu) {
+    public Student(int numarMatricol, String prenume, String nume, String formatieDeStudiu) {
         this.numarMatricol = numarMatricol;
         this.prenume = prenume;
         this.nume = nume;
         this.formatieDeStudiu = formatieDeStudiu;
     }
 
-    public String toString(){
+    public int getNumarMatricol() {
+        return numarMatricol;
+    }
+
+    public String getPrenume(){
+        return prenume;
+    }
+
+    public String getNume(){
+        return nume;
+    }
+
+    public String getFormatieDeStudiu(){
+        return formatieDeStudiu;
+    }
+
+
+   public String toString(){
         return "Nr. Matricol: " +  numarMatricol + "\n" + "Nume: " + nume  + "\n" +"Prenume: " + prenume + "\n" +"Formatie de studiu: " + formatieDeStudiu;
     }
 }
+
--- src/main.java
+++ src/main.java
@@ -1,7 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class main {
+
+    public static boolean existaStudent(List<Student> studenti, Student studentCautat){
+        for(Student s: studenti){
+            if((s.getNume() == studentCautat.getNume()) &&
+                    (s.getPrenume() == studentCautat.getPrenume()) &&
+                    (s.getFormatieDeStudiu() ==  studentCautat.getFormatieDeStudiu()) &&
+                    (s.getNumarMatricol() == studentCautat.getNumarMatricol()) ) return true;
+        }
+        return false;
+    }
+
+
     public static void main(String[] args) {
 
-        Student s1 = new Student(1,"Sergiu","Tanase",1);
-        System.out.println(s1);
+        Student s1 = new Student(128,"Sergiu","Tanase","ISM21/1");
+        Student s2 = new Student(120, "Alis", "Popa", "TI21/2");
+        Student s3 = new Student(112, "Maria", "Popa", "TI21/1");
+        List<Student> studenti = new ArrayList<>();
+        studenti.add(s1);
+        studenti.add(s2);
+        studenti.add(s3);
+
+        for(Student s: studenti){
+            System.out.println(s);
+        }
+
+        System.out.println(existaStudent(studenti,s2));
+        System.out.println(existaStudent(studenti,s3));
+
     }
 }
